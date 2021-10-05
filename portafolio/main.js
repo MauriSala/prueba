23937e4ede24b825b148f964de7f73646ff453f8
MauriSala <90487512+MauriSala@users.noreply.github.com>
2021-10-05 14:44:55 -0500

    subir archivos

---
+++ portafolio/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu-btn').addEventListener('click',()=> {
+
+   document.querySelector('.nav-menu').classList.toggle('show');
+
+})
+
+ScrollReveal().reveal('.showcase',{delay: 250})
+ScrollReveal().reveal('.new-cards',{delay: 500})
+ScrollReveal().reveal('.content',{delay: 500})
+ScrollReveal().reveal('.social',{delay: 500})
+ScrollReveal().reveal('.footer',{delay: 500})
